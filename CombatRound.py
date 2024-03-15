e720ce89df607f415e6addb79d2f91157c97efcd
Wxvxl <metal123cool1115@gmail.com>
2024-03-15 12:26:12 +0800

    Refactor and add comments

--- CombatRound.py
+++ CombatRound.py
@@ -1,24 +1,29 @@
 import random as r
 
-def CombatRound(A, B):
+# Initialize and process combat between units.
+def CombatRound(A, B): # A and B represents two sides.
+    # Fighters are grouped into one list with all of the fighter.
     Fighters = []
     Fighters.extend(A)
     Fighters.extend(B)
+
+    # Sort the fighter by the initiative, then print the combat order. 
     Combat_Order = sorted(Fighters, key=lambda x: x.I, reverse=True)
     print("Combat Begins!")
     for i,fighter in enumerate(Combat_Order):
         print(f"{i+1}. {fighter} at initiative {fighter.I}")
     print()
 
+    # Attacking fighter selects a random enemy target to fight.
     for fighter in Combat_Order:
         if fighter in A:
             target = r.choice(B)
         else:
             target = r.choice(A)
         
-        # rolling to hit
+        # Count the amount rolls that needs to be made.
         for weapon in fighter.weapon_list:
-            rolls_count = fighter.Count * weapon.A
+            rolls_count = fighter.curcount * weapon.A
             hit_rolls = []
             
             # start rolling to hit
@@ -26,46 +31,47 @@ def CombatRound(A, B):
                 hit_rolls.append(r.randint(1,6))
             
             # set the number you need to score a successful hit.
-            hitTarget = 4
+            # Compare the attacker's WS and the defender's WS.
+            hitTarget = 4 # The default target needed to score a hit.
             if (fighter.WS + weapon.HM) > 2 * (target.WS + target.weapon_list[0].HM):
-                hitTarget = 2
+                hitTarget = 2 # if attacker WS is more than double of defender WS, hitting on 2+
             
             elif (fighter.WS + weapon.HM) > (target.WS + target.weapon_list[0].HM):
-                hitTarget = 3
+                hitTarget = 3 # if attacker WS is more than defender WS, hitting on 3+
 
             elif (target.WS + target.weapon_list[0].HM) * 2 > (fighter.WS + weapon.HM):
-                hitTarget = 5
+                hitTarget = 5 # if defender WS is more than double of attacker WS, hitting on 5+
 
             elif ((target.WS + target.weapon_list[0].HM) * 2) + 3 > (fighter.WS + weapon.HM):
-                hitTarget = 6
+                hitTarget = 6 # if defender WS is more than double that of the attacker + 3, then hit on 6+
 
-            
-            print(f"{fighter} attacking {target} using {weapon}")
-            print(f"{fighter} WS: {fighter.WS + weapon.HM}")
-            print(f"{target} WS: {target.WS + target.weapon_list[0].HM}")
+            print(f"{fighter} attacking using {weapon} at {fighter.WS + weapon.HM}WS!")
+            print(f"{target} defending using {target.weapon_list[0]} at {target.WS + target.weapon_list[0].HM}")
             print(f"Roll needed to hit: {hitTarget}+")
-            hit_rolls = [roll for roll in hit_rolls if roll >= hitTarget] 
             
+            # Remove all of the hit roll that does does not meet the hit criteria.
+            hit_rolls = [roll for roll in hit_rolls if roll >= hitTarget] 
             print(f"Successful hit for {fighter} against {target}: {hit_rolls}")
             print()
         
-            # rolling to wound
+            # rolling to wound, works the same way as rolling to hit except successful wound is dictated by Strength and Toughness.
             wound_rolls = []
             print(f"{fighter} rolling to wound against {target} using {weapon} with {weapon.S} strength against {target} with {target.T} toughness")
-            hitTarget = 4
+            hitTarget = 4 # Default value for wounding.
 
             if (weapon.S) >= 2 * (target.T):
-                hitTarget = 2
+                hitTarget = 2 # if Strength more than or equal to double of target Toughness wounding on 2+
             
-            elif (weapon.S) >= (target.T):
-                hitTarget = 3
+            elif (weapon.S) > (target.T):
+                hitTarget = 3 # if Strength higher than toughness, then wounding on 3+
             
             elif (weapon.S) < (target.T):
-                hitTarget = 5
+                hitTarget = 5 # If strength lower than toughness, wounding on 5+
             
-            elif (weapon.S) < (target.T) * 2:
-                hitTarget = 6
+            elif (weapon.S) <= (target.T) * 2:
+                hitTarget = 6 # If strength lower than double of toughness, wounding on 6+!
             
+            # Rolls the wound roll, works the same way as hit roll.
             print(f"roll needed to successfully wound: {hitTarget}+")
             for i in range(len(hit_rolls)):
                 wound_rolls.append(r.randint(1,6))
@@ -73,23 +79,26 @@ def CombatRound(A, B):
             print(f"Successful wounds for {fighter} against {target}: {wound_rolls}")
             print()
 
-            # Saving throws
+            # Units now make their saving throws.
             print(f"{target} rolling for saving throws")
             print(f"{target} has a {target.Sv}+ save, reduced by {fighter}'s {weapon} with AP{weapon.AP}")
 
-            if target.Sv - weapon.AP <= 1:
-                SvTarget = 2
-            else:
-                SvTarget = target.Sv - weapon.AP
+            # Modifies the save by the weapon AP value.
+            SvTarget = target.Sv - weapon.AP
             
+            # Roll the saving throws and remove all of the saved wounds.
             print(f"{target} needs {SvTarget}+ to successfully save a wound!")
             wound_rolls = [roll for roll in wound_rolls if roll < SvTarget]
+
+            # count the total damage dealt.
             damage = (len(wound_rolls)) * weapon.D
             print(f"{target} has {len(wound_rolls)} unsaved wounds, receiving {damage} damage!")
 
+            # counts the amount of casualties that the target unit suffers!
             casualties = damage // target.W
             print(f"{target} suffers {casualties} casualties")
             
+            # Wounds unit that took damage, but did not die.
             unallocated_damage = damage - (casualties * target.W)
             if unallocated_damage != 0:
                 print(f"A {target} is wounded but not slain after suffering {unallocated_damage} damage")
