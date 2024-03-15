644801aaf1e721b59db1d384f573d250c62ccfcb
Wxvxl <metal123cool1115@gmail.com>
2024-03-15 11:57:23 +0800

    Add comments to Core Rules

--- CoreRules.py
+++ CoreRules.py
@@ -1,44 +1,54 @@
+# This initializes the core rules of the game. Such as unit stats, weapon stats, etc.
+# Necessary to import this: "from CoreRules Import X" where X is Weapon, Ability, Unit etc.
 class Weapon: 
     def __init__(self, name, A, HM, S, AP, D, abilities):
         self.name = name
-        self.A = A
-        self.HM = HM
-        self.S = S
-        self.AP = AP
-        self.D = D
-        self.abilities = abilities
+        self.A = A # The attack value of the weapon.
+        self.HM = HM # The "Hit Modifier" of the weapon, modifies the unit WS by this much when attacking.
+        self.S = S # Strength of the weapon, it is compared to the target's toughness when attacking.
+        self.AP = AP # "Armor Piercing" reduce the enemy armor save by this much.
+        self.D = D # Damage, when the weapon successfuly wounds a target it does this much damage.
+        self.abilities = abilities # The list of abilities, this is a list value that contains the "Ability" class.
     
     def __str__(self) -> str:
         return self.name
 
 class Ability:
     def __init__(self, name, description, type):
-        self.name = name
+        self.name = name 
         self.description = description
-        self.type = type
+        self.type = type # The ability type, this is used to check when the ability would fire off.
+        # Current list of types: "Passive", "CombatStart", "CombatEnd", "OnKill", "OnCasualty", "OnDeath"
     
 class Unit:
-    def __init__(self, name, M, WS, BS, T, W, I, LD, Sv, Count):
+    def __init__(self, name, M, WS, BS, T, W, I, LD, Sv, Count, Tags):
+        # TODO: Refactor the unit class to be able to have mixed-model units.
         # Initializes all of the basic stats of the unit
         self.name = name
-        self.M = M
-        self.WS = WS
-        self.BS = BS
-        self.T = T
-        self.W = W
-        self.CW = W
-        self.I = I
-        self.LD = LD
-        self.Sv = Sv
-        self.Count = Count
-        self.weapon_list = []
-        self.abilities_list = []
+        self.M = M # Movement value, unit will move by this much. 
+        self.WS = WS # Weapon Skill, decides how good the unit is at melee combat, helps with hitting more often and getting hit less.
+        self.BS = BS # Ballistic Skill, decides how accurate the unit is when shooting.
+        self.T = T # Toughness, how tough the unit is, compared with the weapon strength to see if the unit will be wounded.
+        self.W = W # Wounds per model.
+        self.CW = W # The current wound of the unit, only used if some unit is partially wounded. 
+        self.I = I # Initiative, decides the order that the unit would strike in, higher initiative is faster.
+        self.LD = LD # Leadership value, decides the morale 
+        self.Sv = Sv # Armor Save, when this unit is successfully wounded they must roll equal or higher to the armor save value to "save" that wound.
+        # Armor save is modified by the attacker's armor piercing.
+        self.caxcount = Count # How many model the unit can have at max 
+        self.curcount = Count # The current count of model in the unit.
+        self.weapon_list = [] # List of weapons that the unit carries. Usually contains only one weapon.
+        self.abilities_list = [] # List of the abilities that the unit has. See ability class.
+        self.tags = [] # Tags associated with the unit, keywords, whether its reinforced,
+        self.statuseffects = [] # Status effects applied to the unit. 
+        # TODO: Create status effects class.
     
     def __str__(self) -> str:
         return self.name
     
     def display_stats(self):
-        print(f"Unit name        : {self.name}")
+        # Displays all of the stats that the unit has.
+        print(f"Unit name        : {self.name}") 
         print(f"Movement         : {self.M}")
         print(f"Weapon Skill     : {self.WS}")
         print(f"Ballistic Skill  : {self.BS}")
@@ -47,13 +57,16 @@ class Unit:
         print(f"Initiative       : {self.I}")
         print(f"Leadership       : {self.LD}")
         print(f"Armour Save      : {self.Sv}+")
-        print(f"Squad Count      : {self.Count}")
+        print(f"Squad Count      : {self.curcount}")
         print()
 
+        # Displays all of the weapon the unit is equipped with. 
+        # Some units may carry multiple weapons.
         print(f'{"Weapon Name":<20}| {"HM":^2} | {"S":^2} | {"AP":^2} | {"D":^1} | {"Abilities":<16}')
         for weapon in self.weapon_list:
             print(f'{weapon.name:<20}| {weapon.A:^2} | {weapon.S:^2} | {weapon.AP:^2} | {weapon.D:^1} | {", ".join(weapon.abilities):^8}')
 
+        # Prints all of the abilities that the unit has and their description.
         if self.abilities_list != []:
             print()
             print(f"{'Ability Name':<20} | {'Description':<30}")
